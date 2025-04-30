2ff09a66fbf0036f10b8dfd739e3a6c75fdd61cb
Nikhil Nagar <nsquarepart1@gmail.com>
2025-04-30 16:26:30 +0530

    fix some issue

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,10 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'build',
-  }
+  },
+  server: {
+    // Enable SPA fallback in dev mode
+    proxy: {},
+    open: true
+  },
 })
