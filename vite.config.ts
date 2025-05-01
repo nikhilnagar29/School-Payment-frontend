08f91918b0f23c7fa5a07fc5589e938932e58425
Nikhil Nagar <nsquarepart1@gmail.com>
2025-05-01 19:14:56 +0530

    try to fix some other bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build',
-  },
   server: {
-    proxy: {},
-    historyApiFallback: true,
+    port: 3000,
     open: true
   },
+  build: { outDir: 'build' }
 })
